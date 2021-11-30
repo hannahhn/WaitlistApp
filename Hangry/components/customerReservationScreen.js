67f5466906edde0cecac9e035c2b495a947806f8
sam3le <samuel.le@mavs.uta.edu>
2021-11-30 02:16:11 -0600

    Added Party Size

--- Hangry/components/customerReservationScreen.js
+++ Hangry/components/customerReservationScreen.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, Button, Alert } from 'react-native';
+import { Text, View, StyleSheet, Button, Alert, Picker } from 'react-native';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import moment from 'moment';
 import ScrollPicker from 'react-native-wheel-scrollview-picker';
 
 
 const customerReservationScreen = () => {
+
+  const [selectedValue, setSelectedValue] = useState("1");
   
   const [date, setDate] = useState(new Date());
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
 
   const showDatePicker = () => {
     setDatePickerVisibility(true);
@@ -46,29 +49,30 @@ const customerReservationScreen = () => {
       <Text style={{ color: 'red' }}>
         {date.toString()}
       </Text>
-      <Button
+      {/* <Button
         title="Choose Party Size"
         onPress={() => Alert.alert('Party Size pressed')}
-      />
-      <ScrollPicker
-        title="Select Party Size"
-        dataSource={['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
-        selectedIndex={1}
-        renderItem={(data, index) => {
-          //
-        }}
-        onValueChange={(data, selectedIndex) => {
-          //
-        }}
-        wrapperHeight={180}
-        wrapperWidth={150}
-        wrapperBackground={'#000'}
-        itemHeight={60}
-        highlightColor={'#d8d8d8'}
-        highlightBorderWidth={2}
-        activeItemColor={'#222121'}
-        itemColor={'#B4B4B4'}
-      />
+      /> */}
+      <Text style={{ color: 'black' }}>
+        Choose Party Size
+      </Text>
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 50, width: 150 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="1" value="1" />
+        <Picker.Item label="2" value="2" />
+        <Picker.Item label="3" value="3" />
+        <Picker.Item label="4" value="4" />
+        <Picker.Item label="5" value="5" />
+        <Picker.Item label="6" value="6" />
+        <Picker.Item label="7" value="7" />
+        <Picker.Item label="8" value="8" />
+        <Picker.Item label="9" value="9" />
+        <Picker.Item label="10" value="10" />
+        <Picker.Item label="More than 10. Please contact Restaurant" value="11" />
+      </Picker>
       <Button
         title="Submit Reservation"
         onPress={() => Alert.alert('Submit Reservation pressed')}
