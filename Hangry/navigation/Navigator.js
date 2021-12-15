a032e34a8f11ce3534dce2af150ad0b6cd95bb26
sam3le <samuel.le@mavs.uta.edu>
2021-12-14 22:20:44 -0600

    removed old files

--- Hangry/navigation/Navigator.js
+++ Hangry/navigation/Navigator.js
@@ -1,14 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
-// import Login from "../components/Login";
-// import SignUp from "../components/SignUp";
-// import Landing from "../components/Landing";
-import BotTabNavigator from "C:/Users/samvi/Desktop/School/Courses/CSE4317/WaitlistApp/Hangry/navigation/BotTabNavigator";
-//import Navigator from "C:/Users/samvi/Desktop/School/Courses/CSE4317/WaitlistApp/Hangry/navigation/Navigator";
-
+import Landing from "../components/LandingScreen";
+import CostumerMenu from "../components/costumerside_menu";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-// import CreateGroup from "../components/CreateGroup";
-// import GroupTask from "../components/GroupTask";
+
 
 
 const Stack = createStackNavigator();
@@ -20,12 +15,8 @@ const Navigator = (props) => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="Home" component={Landing} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SignUp" component={SignUp} /> */}
-        <Stack.Screen name="Home" component={BotTabNavigator} />
-        {/* <Stack.Screen name="CreateGroup" component={CreateGroup} />
-        <Stack.Screen name="GroupTask" component={GroupTask} /> */}
+        <Stack.Screen name="Home" component={Landing} />
+        <Stack.Screen name="Menu" component={CostumerMenu} />
       </Stack.Navigator>
     </NavigationContainer>
   );
