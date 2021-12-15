80d11478b6302541fd2a88b3f950eee15df0f9d8
sam3le <samuel.le@mavs.uta.edu>
2021-12-14 22:32:24 -0600

    modified to Tasked Implementation

--- Hangry/navigation/Navigator.js
+++ Hangry/navigation/Navigator.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect, useContext } from "react";
 import Landing from "../components/LandingScreen";
 import CostumerMenu from "../components/costumerside_menu";
+import RestaurantLoginScreen from '../components/RestaurantLoginScreen'
+import Waitlist from '../components/waitlist';
+import CustomerReservation from '../components/customerReservationScreen';
+import Confirmation from '../components/confirmation';
+import Restaurant_waitlist from '../components/restaurant_waitlist';
+import Restaurant_reservation from '../components/Restaurant_reservation';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
@@ -13,12 +19,34 @@ const Navigator = (props) => {
   
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={Landing} />
-        <Stack.Screen name="Menu" component={CostumerMenu} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator screenOptions={{ headerShown: false }}>
+    //     <Stack.Screen name="Home" component={Landing} />
+    //     <Stack.Screen name="Menu" component={CostumerMenu} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <NavigationContainer initialRouteName="Landing Screen">
+    <Stack.Navigator>
+      <Stack.Screen name="Landing Screen" 
+                    component={Landing} 
+                    options={{
+                      headerShown: false
+                    }}
+      />
+      <Stack.Screen name="Restaurant Login Screen" 
+                    component={RestaurantLoginScreen} 
+                    options={{
+                      headerShown: false
+                    }}
+      />
+      <Stack.Screen name="Home" component={CostumerMenu} />
+      <Stack.Screen name="Waitlist" component={Waitlist} />
+      <Stack.Screen name="Reservation" component={CustomerReservation} />
+      <Stack.Screen name="Confirmation" component={Confirmation} />
+      <Stack.Screen name="Restaurant_waitlist" component={Restaurant_waitlist} />
+      <Stack.Screen name="Restaurant_reservation" component={Restaurant_reservation} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 };
 
